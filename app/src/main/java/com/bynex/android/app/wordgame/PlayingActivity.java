3d3cf43a11685b0068a0ba90d018042ef3127025
Renjith Reghu <renju@renithreghu.com>
2022-07-09 09:38:49 +0530

    implement text to speech, end game, list view bug fixed

--- app/src/main/java/com/bynex/android/app/wordgame/PlayingActivity.java
+++ app/src/main/java/com/bynex/android/app/wordgame/PlayingActivity.java
@@ -6,14 +6,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 
@@ -21,6 +25,8 @@ public class PlayingActivity extends AppCompatActivity {
 
     TextView word, meaning;
     Button play;
+    ImageButton speek;
+    private TextToSpeech tts;
 
 
     @Override
@@ -43,6 +49,7 @@ public class PlayingActivity extends AppCompatActivity {
         play = findViewById(R.id.btn_play);
         word = findViewById(R.id.txt_word_dis);
         meaning = findViewById(R.id.txt_meaning_dis);
+        speek = findViewById(R.id.speek);
 
         if (wordList.size() <= 0) {
             Toast.makeText(getApplicationContext(), "Please add some words", Toast.LENGTH_SHORT).show();
@@ -62,14 +69,39 @@ public class PlayingActivity extends AppCompatActivity {
                     play.setText("Next");
                     wordList.remove(0);
                     Collections.shuffle(wordList);
+                    if (wordList.size() == 0) {
+                        play.setText("End Game");
+                    }
                 } else if (play.getText().toString().equals("Next") && wordList.size() > 0) {
                     meaning.setVisibility(View.INVISIBLE);
                     word.setText(wordList.get(0));
                     meaning.setText(words.getString(wordList.get(0), "null"));
                     play.setText("Show");
-                } else {
-                    Toast.makeText(getApplicationContext(), "Game Over", Toast.LENGTH_SHORT).show();
+                    if (wordList.size() == 0) {
+                        play.setText("End Game");
+                    }
+                } else if (play.getText().toString().equals("End Game")) {
+                    Intent intent = new Intent(PlayingActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
                 }
+            }
+        });
+
+        speek.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                    @Override
+                    public void onInit(int i) {
+                        if (i == TextToSpeech.SUCCESS) {
+                            Toast.makeText(getApplicationContext(), "Playing", Toast.LENGTH_SHORT).show();
+                            tts.setLanguage(Locale.getDefault());
+                            tts.setSpeechRate(1.0f);
+                            tts.speak(word.getText().toString(), TextToSpeech.QUEUE_ADD, null);
+                        }
+                    }
+                });
             }
         });
     }
